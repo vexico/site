0b6d4eb573c13cc24c7a21a3a02cdcfe615043c4
Brendan Lane <me@brndnln.dev>
2020-11-26 12:13:42 -0500

    sex

---
+++ static/js/sex.js
@@ -0,0 +1,21 @@
+let words = [
+    "ass.rest",
+    "sex",
+    "<a href='https://vexi.cc/'>site</a>",
+    "github pages is NOT ass",
+    "spike has big toenails",
+    "honk hosts his software off of a cybernetic rat",
+    "pope jokes about sex",
+    "so much sex",
+    "cocabot gay",
+    "i stuck my peepee in a clarinet",
+    "send help",
+    "voopai gaty",
+    "i need to start a chair company",
+    "spike is about to put a zeroday into spookvooper",
+    "according to all known laws of aviation, there is no way a bee should be able to fly",
+    "send help, i am being held hostage"
+]
+let selectedWord = words[Math.round(Math.random() * words.length - 1)]
+
+document.write(selectedWord)
